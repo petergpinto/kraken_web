940455a43b90e6b2ecea82d76fdb868f6e269f4d
Peter Pinto <peter@peterpinto.dev>
2022-04-24 17:19:11 -0400

    Added AssetInfo.js with two endpoints, Assets and TradablePairs

--- src/AssetInfo.js
+++ src/AssetInfo.js
@@ -1,6 +1,69 @@
-module.export = function (app, pool, util) {
+module.exports = function (app, pool, util) {
 	app.get("/TEST", function (request, response) {
-		response.end("TEST")
+		response.end("TEST");
+	})
+
+	app.get("/api/TradablePairs", async function(request, response) {
+		if(!request.session.loggedin) {
+			response.json({"Result":"Error, please login"})
+			return
+		}
+		if(request.query.pairs) {
+			getTradablePairsPromise = () => {
+				return new Promise((resolve, reject) => {	
+                pool.query('SELECT * FROM tradable_pairs WHERE pair in (?)', [request.query.pairs.split(',')],
+                    (error, elements) => {
+                        if(error) return reject(error);
+						return resolve(elements);
+                    });
+                });
+	        }
+		} else {
+			getTradablePairsPromise = () => {
+                return new Promise((resolve, reject) => {
+                pool.query('SELECT * FROM tradable_pairs',
+                    (error, elements) => {
+                        if(error) return reject(error);
+						return resolve(elements);
+                    });
+                });
+            }
+		}
+
+		response.json(await getTradablePairsPromise());
+
+	});
+
+	app.get("/api/Assets", async function(request, response) {
+		if(!request.session.loggedin) {
+            response.json({"Result":"Error, please login"})
+            return
+        }
+
+		if(request.query.assets) {
+            getAssetsPromise = () => {
+                return new Promise((resolve, reject) => {
+                pool.query('SELECT * FROM assets WHERE asset in (?)', [request.query.assets.split(',')],
+                    (error, elements) => {
+                        if(error) return reject(error);
+                        return resolve(elements);
+                    });
+                });
+            }
+        } else {
+            getAssetsPromise = () => {
+                return new Promise((resolve, reject) => {
+                pool.query('SELECT * FROM assets',
+                    (error, elements) => {
+                        if(error) return reject(error);
+                        return resolve(elements);
+                    });
+                });
+            }
+        }
+
+		response.json(await getAssetsPromise());
+
 	})
 }
 
