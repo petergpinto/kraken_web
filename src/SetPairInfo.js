76a5020eb4aba3c48781df3bbccf54bc16d592e5
Peter Pinto <peter@peterpinto.dev>
2022-04-24 17:30:48 -0400

    Added endpoint to set doTrading on pairs

---
+++ src/SetPairInfo.js
@@ -0,0 +1,31 @@
+module.exports = function (app, pool, util) {
+	app.post("SetTradingStatus", async function(request, response) {
+		if(!request.session.loggedin) {
+            response.json({"Result":"Error, please login"})
+            return
+        }
+
+		let pair = request.body.pair;
+		let pairStatus = request.body.pairStatus;
+
+		if(!pair || !pairStatus) {
+			response.json({"Result":"Invalid Request"});
+		}
+
+		updateStatusPromise = () => {
+			return new Promise ((resolve, reject) => {
+				pool.query("UPDATE trading_pairs SET doTrading=? WHERE pair=?", [pairStatus, pair],
+					(error, elements) => {
+						if(error) return resolve(false);
+						return resolve(true);
+					});
+				});
+		}
+
+		let result = await updateStatusPromise();
+
+		if(result)
+			response.json({"Result":"Success"});
+		else
+			response.json({"Result":"Failure"});
+	})
